e7a366d3c9be4d4ba4429bf5d9abad1a0cd9dc07
Aluno <goes4754@gmail.com>
2019-03-12 19:03:29 -0300

    3.2

--- acao/cadastrando.php
+++ acao/cadastrando.php
@@ -5,27 +5,25 @@
 </head>
 <body>
 <?php
-$host ="localhost";
-$user ="";
-$pass ="";
-$banco ="escola";
-$conexao = mysql_connect($host, $user, $pass) or die(mysql_error());
-mysql_select_bd($banco) or die(mysql_error());
+$db = NULL;
+$conexao = NULL;
+$conexao = mysqli_connect('localhost','root','') or die(mysqli_error());
+$db = mysqli_select_db($conexao,'escola');
+
 
 ?>
 
 <?php
-
+$id = NULL;
 $nome=$_POST['nome'];
 $telefone=$_POST['telefone'];
 $email=$_POST['email'];
 $sexo=$_POST['sexo'];
-$dataNascimento=$_POST['dataNascimento'];
+$dataNascimento= $_POST['dataNascimento'];
 $senha=$_POST['senha'];
-$sql = mysql_query("INSERT INTO usuarios(nome, telefone, email, sexo, datanascimento, senha)
-VALUES('$nome','$telefone','$email','$sexo','$dataNascimento','$senha')");
+$query = mysqli_query($conexao,"INSERT INTO usuario VALUES ('$id','$nome','$telefone','$email','$sexo','$dataNascimento','$senha')");
 
-echo"<center><h1>Cadastro efetuado comsucesso!!<h1>"
+echo"<center><h1>Cadastro efetuado com sucesso!!<h1>";
 
 ?>
 </body>
